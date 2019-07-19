7fd10ac2ec6c0c126b59328ec6680a221a384559
wangyongfeng <wangyongfeng@ultrapower.com.cn>
2019-07-20 00:16:44 +0800

    修改env地址

--- src/config/env.js
+++ src/config/env.js
@@ -4,8 +4,8 @@
  * baseUrl: 域名地址
  * imgBaseUrl: 图片所在域名地址
  */
-let baseUrl = '/api';//本地环境地址
-// let baseUrl = 'http://123.126.34.176:20102/';//测试环境地址
+// let baseUrl = '/api';//本地环境地址
+let baseUrl = 'http://123.126.34.176:20102/';//测试环境地址
 let imgBaseUrl = '';//生产环境图片地址
 
 
